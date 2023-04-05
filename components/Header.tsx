09002a70857f80821d29091fe477962ca2a6216c
HIROE Kouta <hiroekota114@gmail.com>
2023-04-05 19:48:14 +0900

    ホームページ，ヘッダーをおしゃれに

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,31 @@
-// components/Header.tsx
-import { AppBar, Button, Toolbar } from "@mui/material";
-import Link from "next/link";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { useRouter } from "next/router";
 
-const Header: React.FC = () => {
+const Header = () => {
+  const router = useRouter();
+
+  const handleClick = (path: string) => {
+    router.push(path);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Link href="/">
-          <Button color="inherit">ポートフォリオ</Button>
-        </Link>
-        <Link href="/blog">
-          <Button color="inherit">ブログ</Button>
-        </Link>
-        <Link href="/contact">
-          <Button color="inherit">コンタクト</Button>
-        </Link>
-        <Link href="/other">
-          <Button color="inherit">その他</Button>
-        </Link>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          My Portfolio
+        </Typography>
+        <Button color="inherit" onClick={() => handleClick("/")}>
+          ホームページ
+        </Button>
+        <Button color="inherit" onClick={() => handleClick("/blog")}>
+          ブログページ
+        </Button>
+        <Button color="inherit" onClick={() => handleClick("/contact")}>
+          コンタクトページ
+        </Button>
+        <Button color="inherit" onClick={() => handleClick("/others")}>
+          その他ページ
+        </Button>
       </Toolbar>
     </AppBar>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,77 @@
 // pages/index.tsx
-import Introduction from "../components/Introduction";
-import Skills from "../components/Skills";
-import Career from "../components/Career";
+import { Box, Typography, Button } from "@mui/material";
 import Header from "../components/Header";
 
 const IndexPage: React.FC = () => {
   return (
     <>
       <Header />
-      <div>
-        <Introduction />
-        <Skills />
-        <Career />
-      </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "calc(100vh - 64px)", // ヘッダーの高さを考慮して計算
+          backgroundImage: `url('/bg.jpg')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          color: "#fff",
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h1" sx={{ mb: 2 }}>
+          Hello, World!
+        </Typography>
+        <Typography variant="h4" sx={{ mb: 4 }}>
+          私のポートフォリオへようこそ
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Button variant="contained" color="primary">
+            ポートフォリオページへ
+          </Button>
+          <Button variant="contained" color="secondary">
+            ブログページへ
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          自己紹介
+        </Typography>
+        <Typography>
+          私は〇〇県出身の〇〇です。現在はWebエンジニアとして活躍しています。
+        </Typography>
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          スキルセット
+        </Typography>
+        <Typography>
+          HTML/CSS, JavaScript, TypeScript, React, Next.js, Node.js, MongoDB
+        </Typography>
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          職歴
+        </Typography>
+        <Typography>
+          〇〇株式会社 （2019年4月〜現在）
+          <br />
+          Webエンジニアとして、フロントエンド開発に従事。
+        </Typography>
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <Typography variant="h4" sx={{ mb: 2 }}>
+          お問い合わせ
+        </Typography>
+        <Typography>お問い合わせはこちらからお願いいたします。</Typography>
+      </Box>
     </>
   );
 };
